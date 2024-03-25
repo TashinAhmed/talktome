3d5a2398ae85b6564985532f7c138fa2cfe9074d
Tashin Ahmed <tashinahmed@aol.com>
2024-03-26 04:08:17 +0600

    black formatter implemented

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-import  subprocess
+import subprocess
 import urllib.parse
 import shlex
 
 query = input("Question: ")
 
-encodedquery =  urllib.parse.quote(query)
+encodedquery = urllib.parse.quote(query)
 command = f"curl -X 'GET' 'http://127.0.0.1:8000/lamini?question={encodedquery}' -H 'accept: application/json'"
 
 args = shlex.split(command)
-process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process = subprocess.Popen(
+    args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+)
 stdout, stderr = process.communicate()
 print(stdout)
